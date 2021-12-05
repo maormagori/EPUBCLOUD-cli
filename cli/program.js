9a8372f78c27f608015eedc31a18a11df0ca7ef7
Maor Magori <maor@magori.online>
2021-12-05 22:25:45 +0200

    fix: 🚑️ Fixed cli options and requirements

--- cli/program.js
+++ cli/program.js
@@ -2,18 +2,17 @@ const { program } = require("commander");
 
 program
     .requiredOption(
-        "-e, --email <email>]",
+        "-e, --email <email>",
         "epubcloud account email is necessary."
     )
     .requiredOption(
         "-p, --password <password>",
         "epubcloud account password is necessary."
     )
-    .option("-d, --directory <dir>", "EPUB files download directory.")
+    .requiredOption("-d, --directory <dir>", "EPUB files download directory.")
     .option("-a, --all", "Download all of user's available books.", false)
 
     //Uninitialized but in planning.
-    .option("-k, --kindle", "Kindle address to send the books to.")
     .option("-c, --convert", "Choose books files' extension.");
 
-program.parse();
+module.exports = program;
