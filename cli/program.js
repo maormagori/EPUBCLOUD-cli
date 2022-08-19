ba3c12e9984f7b18353c25aa53977ca69816bb27
Maor Magori <maor@magori.online>
2022-08-19 23:15:06 +0300

    fix: ✨ Add hbreader API-Key as const
    As per #10
    
    API key is no longer a required option

--- cli/program.js
+++ cli/program.js
@@ -1,16 +1,15 @@
 const { program } = require("commander");
 
 program
-    .requiredOption(
-        "-e, --email <email>",
-        "epubcloud account email is necessary."
-    )
-    .requiredOption(
-        "-p, --password <password>",
-        "epubcloud account password is necessary."
-    )
-    .requiredOption("-d, --directory <dir>", "EPUB files download directory.")
-    .requiredOption("-ak, --apikey <apikey>", "Your hbreader apikey.")
-    .option("-a, --all", "Download all of user's available books.", false);
+  .requiredOption(
+    "-e, --email <email>",
+    "epubcloud account email is necessary."
+  )
+  .requiredOption(
+    "-p, --password <password>",
+    "epubcloud account password is necessary."
+  )
+  .requiredOption("-d, --directory <dir>", "EPUB files download directory.")
+  .option("-a, --all", "Download all of user's available books.", false);
 
 module.exports = program;
